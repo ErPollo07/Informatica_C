baeb320898a96a7a80ba9137c7001e723e6bda56
ErPolloSgravato <nicola.tosatti@iisviolamarchesini.edu.it>
2024-01-09 11:03:03 +0100

    Changed comment and variable name in Es2

--- Esercizio2.c
+++ Esercizio2.c
@@ -11,36 +11,36 @@ Su una somma di denaro (relativa all'acquito di un prodotto) si vuole applicare 
 
 void main() {
     
-    float prezzo, prezzoScontato;
-    float sconto, sconto1000 = 20, scontoOltre1001 = 30;
-    int continua;
+    float price, discountedPrice;
+    float discount;
+    int _continue;
 
     do {
-        // Chiedo l'input fino a che il prezzo < 0
+        // Ask the input until it's > 0
         do {
             printf("inserire l'importo: ");
-            scanf("%f", &prezzo);
-        } while (prezzo < 0);
+            scanf("%f", &price);
+        } while (price < 0);
 
 
-        // Calcolo lo sconto e lo applico al prezzo
-        if (prezzo < 500) {
-            sconto = 10;
-            prezzoScontato = prezzo - prezzo * (sconto / 100.0);
+        // Calculate the discount based on the price
+        if (price < 500) {
+            discount = 10;
+            discountedPrice = price - price * (discount / 100.0);
         }
-        else if (prezzo >= 500 && prezzo < 1001) {
-            sconto = 20;
-            prezzoScontato = prezzo - prezzo * (sconto / 100.0);
+        else if (price >= 500 && price < 1001) {
+            discount = 20;
+            discountedPrice = price - price * (discount / 100.0);
         }
-        else if (prezzo >= 1001) {
-            sconto = 30;
-            prezzoScontato = prezzo - prezzo * (sconto / 100.0);
+        else if (price >= 1001) {
+            discount = 30;
+            discountedPrice = price - price * (discount / 100.0);
         }
 
-        // Stampo il prezzo inserito dall'utente e il prezzo con lo sconto applicato
-        printf("Importo inseririto: %.2f\nimporto con sconto: %.2f\n", prezzo, prezzoScontato);
+        // Display amount 
+        printf("Importo inseririto: %.2f\nimporto con sconto: %.2f\n", price, discountedPrice);
 
         printf("Vuoi inserire un altro importo: \n(1) SI\n(0) NO\n");
-        scanf("%d", &continua);
-    } while (continua);
+        scanf("%d", &_continue);
+    } while (_continue);
 }
